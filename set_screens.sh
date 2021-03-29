3e201f1cc712f50f0657afcee9f83f24c682e016
jtw023 <R3stock3r@tutanota.com>
2021-03-29 13:32:45 -0700

    updated script

--- set_screens.sh
+++ set_screens.sh
@@ -7,4 +7,5 @@ sleep 2
 vim -c wq ~/.config/picom/picom.conf
 sleep 2
 redshift -l 36.17497:-115.13722 -P -O 1900
-python3 ~/.config/qtile/scripts/pywal-colors.py
+nitrogen --random --set-scaled --head=0
+nitrogen --random --set-scaled --head=1
