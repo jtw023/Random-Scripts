8212d472326f5540012e81792d085201ddbe7085
jtw023 <r3stock3r@tutanota.com>
2021-04-27 14:24:19 -0700

    added script to set screens using xrandr

---
+++ set_screens.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+xrandr --output "DP-1-0" --primary --mode 1920x1080 --rate 60.00 --output "eDP-1" --mode 1920x1080 --rate 144.00 --right-of "DP-1-0" --output "DP-1-2" --mode 1600x900 --rate 59.95 --left-of "DP-1-0"
