949bce293a47796c6c1a045f5fe4d536a01718ba
jtw023 <r3stock3r@tutanota.com>
2021-04-30 13:47:40 -0700

    modified script to include rofi and two different directories

--- music_player.sh
+++ music_player.sh
@@ -3,10 +3,23 @@
 # This is a script for launching mpv as a music player.
 #
 # What I use it for: I have a keybinding to this script which
-# launches a terminal, runs mpv with the shuffle command in my
-# music directory, and then closes the terminal while mpv is
-# still playing.
+# launches a terminal, promps with rofi which directory to
+# source music from, and runs mpv with the shuffle command in
+# that music directory. Then it also closes the terminal while
+# mpv is playing.
 #
-# Tools used: devour, mpv.
+# Tools used: devour, rofi, and mpv.
 
-devour mpv --no-resume-playback --shuffle /home/$USER/download/Playlist/all\ songs/
+MUSIC_LIST=("Lyrical Playlist" "Instrumental Playlist")
+
+CHOICE=$(printf '%s\n' "${MUSIC_LIST[@]}" | rofi -dmenu -i -p "Choose a playlist: ")
+
+if [[ $CHOICE = "Lyrical Playlist" ]]; then
+
+    devour mpv --no-resume-playback --shuffle /home/$USER/download/Playlist/lyrical\ music/
+
+elif [[ $CHOICE = "Instrumental Playlist" ]]; then
+
+    devour mpv --no-resume-playback --shuffle /home/$USER/download/Playlist/instrumental\ music/
+
+fi
