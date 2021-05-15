a2b46e02332de210409980be6fbd9db28a7fee87
jtw023 <r3stock3r@tutanota.com>
2021-05-14 22:06:55 -0700

    added descriptive comments

--- trim.sh
+++ trim.sh
@@ -3,8 +3,17 @@
 # This is a script for triming audio with ffmpeg.
 #
 # What I use it for: it simply automates my ffmpeg command so I
-# don't have to remember it.
+# don't have to remember it. I also have it aliased to 'trim'
+# inside of my .zshrc
 #
 # Tools used: ffmpeg, bash.
+#
+# Enter the arguments inside the same directory as the song to
+# trim.
+#
+# Arguments: 1 is the file to trim, 2 is the start time, 3 is
+# the end time, 4 is a filename for the new version.
+#
+# Be sure to wrap all arguments inside of double quotes.
 
 bash -c "ffmpeg -i /home/jordan/Music/lyrical\\ music/$1 -ss $2 -to $3 -c copy /home/jordan/Music/lyrical\\ music/$4"
