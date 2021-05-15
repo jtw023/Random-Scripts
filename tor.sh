730482844339a3a496639a7822e2efd38fdd591e
jtw023 <r3stock3r@tutanota.com>
2021-05-14 21:55:02 -0700

    added descriptive comments

--- tor.sh
+++ tor.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
+#
+# This is a script to see my torrent status.
+#
+# What I use it for: after adding an iso to my torrent
+# client I run this script to check on the status.
+#
+# Tools used: clear, transmission-remote, sleep.
+
+alacritty -e transmission-remote -l
 
 while [ true ]
 do
