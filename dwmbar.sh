d4bf0a107556ae877c3df03641b22f84019a6170
jtw023 <r3stock3r@tutanota.com>
2022-04-27 16:21:40 -0700

    Removed seconds from time.

--- dwmbar.sh
+++ dwmbar.sh
@@ -6,7 +6,7 @@ if [ $time != '00' ]; then
 
 	sleep $(expr 60 - $time)
 	while true; do
-		xsetroot -name "$(date +'%a %b %I:%M:%S %p') \
+		xsetroot -name "$(date +'%a %b %I:%M %p') \
 | Battery: $(acpi | awk '{print $4}' | sed s/,// | head -n 1) \
 | Updates: $(checkupdates | wc -l)  "
 		sleep 59
