6a9283816a50e148f7f762459e45a78cb68cc9ed
jtw023 <r3stock3r@tutanota.com>
2022-04-27 14:27:13 -0700

    Made script to set dwm bar.

---
+++ dwmbar.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+time=$(date +%S)
+
+if [ $time != '00' ]; then
+
+	sleep $(expr 60 - $time)
+	while true; do
+		xsetroot -name "$(date +'%a %b %I:%M:%S %p') \
+| Battery: $(acpi | awk '{print $4}' | sed s/,// | head -n 1) \
+| Updates: $(checkupdates | wc -l)  "
+		sleep 59
+	done
+
+else
+
+	while true; do
+		xsetroot -name "$(date +'%a %b %I:%M:%S %p') \
+| Battery: $(acpi | awk '{print $4}' | sed s/,// | head -n 1) \
+| Updates: $(checkupdates | wc -l)  "
+		sleep 59
+	done
+
+fi
