0c95a5214e9dc29c53e242e8c25da52087917c4b
Jordan Walters <jordan.walters@reflexmedia.com>
2023-05-03 04:17:08 -0700

    Updated scripts and fleshed them into a more fully-featured program

--- create_git_repo.sh
+++ create_git_repo.sh
@@ -7,15 +7,110 @@
 # What I use it for: I have it aliased in my zshrc as 'create' for creating
 # a github repository.
 
-# Tools used: gh, mkdir, cd, git, touch, echo
+# Tools used: gh, mkdir, cd, git, touch, echo, exit, break, read, select
 
-DIRECTORY_NAME="/Users/jordanwalters/github_repos/$1"
+CURRENT_DIRECTORY=$(pwd)
 
-mkdir -pv "$DIRECTORY_NAME"
-cd "$DIRECTORY_NAME"
-git init
-touch README.md
-echo "Initialized Repository" >> README.md
-git add .
-git commit -m "Initial Commit"
-gh repo create "$1" --public --source=. --remote=upstream --push
+echo -e "\nWhat would you like to do with this new repository?"
+select CHOICE1 in "Create Inside New Directory" "Create Inside Existing Directory" "Exit"; do
+   if [[ $CHOICE1 == "Exit" ]]; then
+       exit 0
+   fi
+   break
+done
+
+
+if [[ $CHOICE1 == "Create Inside Existing Directory" ]]; then
+    echo -e "\nWould you like this new repository to be in the home folder or current directory?"
+    select CHOICE2 in "Home Folder" "Current Directory" "Exit"; do
+       if [[ $CHOICE2 == "Exit" ]]; then
+           exit 0
+       fi
+        break
+    done
+    if [[ $CHOICE2 == "Home Folder" ]]; then
+        if [[ ! -f "$HOME/$1" ]] && [[ ! -d "$HOME/$1" ]]; then
+            echo -e "\e[32m✓ \e[0m Creating new repo called $HOME/$1"
+            mkdir -p "$HOME/$1"
+            cd "$HOME/$1"
+            git init
+            touch README.md
+            echo "Repository Initialized" >> README.md
+            git add .
+            git commit -m "Initial Commit"
+            gh repo create "$1" --public --source=. --remote=upstream --push
+            echo -e "\e[32m✓ \e[0m Repo created, exiting."
+            exit 0
+        else
+            echo -e "\e[31m \e[0m That name already exists in the home folder, exiting."
+            exit 0
+        fi
+    fi
+    if [[ $CHOICE2 == "Current Directory" ]]; then
+        if [[ ! -f "$CURRENT_DIRECTORY/$1" ]] && [[ ! -d "$CURRENT_DIRECTORY/$1" ]]; then
+            echo -e "\e[32m✓ \e[0m Creating new repo called $CURRENT_DIRECTORY/$1"
+            mkdir -p "$CURRENT_DIRECTORY/$1"
+            cd "$CURRENT_DIRECTORY/$1"
+            git init
+            touch README.md
+            echo "Repository Initialized" >> README.md
+            git add .
+            git commit -m "Initial Commit"
+            gh repo create "$1" --public --source=. --remote=upstream --push
+            echo -e "\e[32m✓ \e[0m Repo created, exiting."
+            exit 0
+        else
+            echo -e "\e[31m \e[0m That name already exists in the current folder, exiting."
+            exit 0
+        fi
+    fi
+elif [[ $CHOICE1 == "Create Inside New Directory" ]]; then
+    echo -e "\nWould you like this new directory to be in the home folder or current directory?"
+    select CHOICE2 in "Home Folder" "Current Directory" "Exit"; do
+       if [[ $CHOICE2 == "Exit" ]]; then
+           exit 0
+       fi
+        break
+    done
+    read -rep $'Name your new directory:\n' DIRECTORY
+    if [[ $CHOICE2 == "Home Folder" ]]; then
+        if [[ ! -f "$HOME/$DIRECTORY" ]] && [[ ! -d "$HOME/$DIRECTORY" ]]; then
+            echo -e "\e[32m✓ \e[0m Making new folder called $HOME/$DIRECTORY"
+            mkdir -p "$HOME/$DIRECTORY"
+            echo -e "\e[32m✓ \e[0m Creating new repo called $HOME/$DIRECTORY/$1"
+            mkdir -p "$HOME/$DIRECTORY/$1"
+            cd "$HOME/$DIRECTORY/$1"
+            git init
+            touch README.md
+            echo "Repository Initialized" >> README.md
+            git add .
+            git commit -m "Initial Commit"
+            gh repo create "$1" --public --source=. --remote=upstream --push
+            echo -e "\e[32m✓ \e[0m Repo created, exiting."
+            exit 0
+        else
+            echo -e "\e[31m \e[0m That name already exists in the home folder, exiting."
+            exit 0
+        fi
+    fi
+    if [[ $CHOICE2 == "Current Directory" ]]; then
+        if [[ ! -f "$CURRENT_DIRECTORY/$DIRECTORY" ]] && [[ ! -d "$CURRENT_DIRECTORY/$DIRECTORY" ]]; then
+            echo -e "\e[32m✓ \e[0m Making new folder called $CURRENT_DIRECTORY/$DIRECTORY"
+            mkdir -p "$CURRENT_DIRECTORY/$DIRECTORY"
+            echo -e "\e[32m✓ \e[0m Creating new repo called $CURRENT_DIRECTORY/$DIRECTORY/$1"
+            mkdir -p "$CURRENT_DIRECTORY/$DIRECTORY/$1"
+            cd "$CURRENT_DIRECTORY/$DIRECTORY/$1"
+            git init
+            touch README.md
+            echo "Repository Initialized" >> README.md
+            git add .
+            git commit -m "Initial Commit"
+            gh repo create "$1" --public --source=. --remote=upstream --push
+            echo -e "\e[32m✓ \e[0m Repo created, exiting."
+            exit 0
+        else
+            echo -e "\e[31m \e[0m That name already exists in the current folder, exiting."
+            exit 0
+        fi
+    fi
+fi
--- delete_git_repo.sh
+++ delete_git_repo.sh
@@ -1,18 +1,102 @@
 #!/usr/bin/env bash
 
+# NOTICE: This program uses the command `rm -rf`. Please, Please, Please be
+# careful what you run.
+
 # This is a script that takes a git repo, deletes the repo on github,
 # and then also deletes the repo on the local computer.
 
 # What I use it for: I have it aliased in my zshrc as 'delete' for deleting
 # a github repository.
 
-# Tools used: gh, sleep, echo, curl, jq, rm
+# Tools used: gh, sleep, echo, curl, jq, rm, sed
 
-gh repo delete "$1" --yes
-sleep 2
-echo "Seeing if directory can safely be removed locally."
-if [[ $(curl -s "https://api.github.com/repos/jtw023/$1" | jq -r ".message") == "Not Found" ]]; then
-	echo "Removing directory."
-	/bin/rm -rf "$1"
-	echo "Directory removed."
+echo -e "\nAre you sure you want to delete the remote git repo and the associated local directory?"
+select CHOICE1 in "Yes" "No" "Exit"; do
+    if [[ $CHOICE1 == "Exit" ]]; then
+        echo -e "\e[31m \e[0m Script quit, exiting."
+        exit 0
+    fi
+    break
+done
+
+if [[ $CHOICE1 == "Yes" ]]; then
+    CURRENT_DIRECTORY=$(pwd)
+    DIRECTORY_TO_DELETE="$CURRENT_DIRECTORY/$1"
+
+    cd $DIRECTORY_TO_DELETE
+
+    GIT_URL1=$(git config remote.origin.url)
+    GIT_URL2=$(git config remote.upstream.url)
+
+    cd $CURRENT_DIRECTORY
+
+    if [[ $GIT_URL1 ]]; then
+        GIT_URL="$GIT_URL1"
+        MODIFICATIONS=${GIT_URL1##*:}
+        GIT_REPO=$(echo $MODIFICATIONS | sed 's/.git$//')
+    elif [[ $GIT_URL2 ]]; then
+        GIT_URL="$GIT_URL2"
+        MODIFICATIONS=${GIT_URL2##*:}
+        GIT_REPO=$(echo $MODIFICATIONS | sed 's/.git$//')
+    fi
+
+    if [[ $GIT_URL == *"github"* ]]; then
+        echo -e "\n"
+        echo -e "\e[32m✓ \e[0m Please confirm remote repo name: $GIT_REPO"
+        select CHOICE2 in "Yes" "No" "Exit"; do
+            if [[ $CHOICE2 == "Exit" ]]; then
+                echo -e "\e[31m \e[0m Script stopped prior to delete, exiting."
+                exit 0
+            fi
+            break
+        done
+        if [[ $CHOICE2 == "Yes" ]]; then
+            if [[ $(curl -s "https://api.github.com/repos/$GIT_REPO" | jq -r ".message") != "Not Found" ]]; then
+                echo -e "\e[32m✓ \e[0m Removing remote repository."
+                gh repo delete "$GIT_REPO" --yes
+            else
+                echo -e "\e[32m✓ \e[0m Remote repository already deleted."
+            fi
+        else
+            echo -e "\e[31m \e[0m Script stopped prior to delete, exiting."
+            exit 0
+        fi
+        echo -e "\e[32m✓ \e[0m Checking if local repo can safely be removed."
+        sleep 2
+        if [[ $(curl -s "https://api.github.com/repos/$GIT_REPO" | jq -r ".message") == "Not Found" ]]; then
+            echo -e "\e[32m✓ \e[0m Please confirm local directory: $DIRECTORY_TO_DELETE"
+            select CHOICE3 in "Yes" "No" "Exit"; do
+                if [[ $CHOICE3 == "Exit" ]]; then
+                    echo -e "\e[31m \e[0m Script stopped prior to delete, exiting."
+                    exit 0
+                fi
+                break
+            done
+            if [[ $CHOICE3 == "Yes" ]]; then
+                echo -e "\e[32m✓ \e[0m Removing local directory."
+                /bin/rm -rf "$DIRECTORY_TO_DELETE"
+            else
+                echo -e "\e[31m \e[0m Script stopped prior to delete, exiting."
+                exit 0
+            fi
+            if [[ ! -d "$DIRECTORY_TO_DELETE" ]]; then
+                echo -e "\e[32m✓ \e[0m Local directory removed, script finished."
+                exit 0
+            else
+                echo -e "\e[31m \e[0m Deletion failed, exiting."
+                exit 0
+            fi
+        else
+            echo -e "\e[31m \e[0m Remote repo on github still exists and the associated directory should not be removed locally."
+            echo -e "\e[31m \e[0m Please delete remote repo first."
+            exit 0
+        fi
+    elif [[ $GIT_URL ]]; then
+        echo -e "\e[31m \e[0m This script only supports deletion of github repos, exiting."
+        exit 0
+    else
+        echo -e "\e[31m \e[0m This is not a github or bitbucket git repo, exiting."
+        exit 0
+    fi
 fi
